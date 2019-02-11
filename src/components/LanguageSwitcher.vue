d6533af5bff2e64f01816ae27b295ca154d28a87
Emmanuel Rafitosoa <emmanuelrafitosoa@MacBook-Pro-de-Emmanuel.local>
2019-02-11 23:51:36 +0100

    RSVP form Done

--- src/components/LanguageSwitcher.vue
+++ src/components/LanguageSwitcher.vue
@@ -1,11 +1,14 @@
 <template>
-	<div class="LanguageSwitcher dropdown">
-		<button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-			{{ lang }}
+	
+	<div class="LanguageSwitcher btn-group">
+		<button type="button" class="btn btn-default dropdown-toggle gla_wht_txt" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+		<span v-bind:class="['flag-icon',currentLanguageFlag]"></span> <span class="caret"></span>
 		</button>
-		<div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-			<a v-for="lang in languages" href="#" v-on:click="changeLanguage(lang)" v-bind:class="{ active: isCurrentLanguage(lang) }">{{lang}}</a>
-		</div>
+		<ul class="dropdown-menu">
+			<li v-for="lang in languages">
+				<a href="#" v-on:click="changeLanguage(lang)" ><span v-bind:class="['flag-icon',getLangagueFlag(lang)]"></span></a>
+			</li>
+		</ul>
 	</div>
 
 </template>
@@ -19,6 +22,9 @@ export default {
   computed: {
     currentLanguage () {
       return this.$i18n.locale
+    },
+    currentLanguageFlag (){
+    	return "flag-icon-"+this.$i18n.locale
     }
   },
   methods: {
@@ -39,6 +45,9 @@ export default {
     },
     isCurrentLanguage (lang) {
       return lang === this.currentLanguage
+    },
+    getLangagueFlag(lang){
+    	return "flag-icon-"+lang
     }
   }
 }
@@ -47,5 +56,87 @@ export default {
 <style>
 .LanguageSwitcher {
   margin-bottom: 1rem;
+  margin-top: 7px;
 }
+
+.btn.dropdown-toggle {
+	padding-left: 10px;
+	padding-right: 10px;
+	border-radius: 5px;
+	padding: 6px 10px;
+}
+
+.btn-group > .btn:first-child {
+
+    margin-left: 0;
+
+}
+.btn-group-vertical > .btn, .btn-group > .btn {
+
+    position: relative;
+    float: left;
+
+}
+.dropdown-menu {
+    position: absolute;
+    top: 100%;
+    left: 0;
+    z-index: 1000;
+    display: none;
+    float: left;
+    min-width: 40px;
+    padding: 5px 0;
+    margin: 2px 0 0;
+    font-size: 14px;
+    text-align: left;
+    list-style: none;
+    background-color: #fff;
+    -webkit-background-clip: padding-box;
+    background-clip: padding-box;
+    border: 1px solid #ccc;
+    border: 1px solid rgba(0,0,0,.15);
+    border-radius: 4px;
+    -webkit-box-shadow: 0 6px 12px rgba(0,0,0,.175);
+    box-shadow: 0 6px 12px rgba(0,0,0,.175);
+}
+
+.dropdown-menu > li > a {
+
+    display: block;
+    padding: 3px 10px;
+    clear: both;
+    font-weight: 400;
+    line-height: 1.42857143;
+    color: #333 !important;
+    white-space: nowrap;
+
+}
+
+.flag-icon-background {
+  background-size: contain;
+  background-position: 50%;
+  background-repeat: no-repeat;
+}
+.flag-icon {
+  background-size: contain;
+  background-position: 50%;
+  background-repeat: no-repeat;
+  position: relative;
+  display: inline-block;
+  width: 1.33333333em;
+  line-height: 1em;
+}
+.flag-icon:before {
+  content: "\00a0";
+}
+.flag-icon.flag-icon-squared {
+  width: 1em;
+}
+.flag-icon-fr {
+  background-image: url("/static/images/fr.svg");
+}
+.flag-icon-en {
+  background-image: url("/static/images/gb-eng.svg");
+}
+
 </style>
--- src/components/sections/Rsvp.vue
+++ src/components/sections/Rsvp.vue
@@ -4,44 +4,51 @@
             <p><img src="/static/images/animations/rsvp.gif" data-bottom-top="@src:/static/images/animations/rsvp.gif" height="180" alt=""></p>
             <div class="row" style="">
                 <div class="col-md-8 col-md-push-2">
-                    <form action="https://formspree.io/your@mail.com" method="POST">
+                    <form v-bind:id="'newAnswerForm_'+newAnswer['.key']" class="form" v-on:submit.prevent="checkForm">
+                        <p v-if="errors.length" class="alert alert-warning" align="left">
+                            <b>Please correct the following error(s):</b>
+                            <ul>
+                              <li v-for="error in errors">{{ error }}</li>
+                            </ul>
+                        </p>
                         <div class="row">
                             <div class="col-md-6">
-                                <label v-html="$t('rsvp.form.firstname')"></label>
-                                <input type="text" name="firstname" class="form-control form-opacity">
+                                <label v-bind:for="'answerFirstname_'+newAnswer['.key']" v-html="$t('rsvp.form.firstname')"></label>
+                                <input v-bind:id="'answerFirstname_'+newAnswer['.key']" type="text" name="firstname" class="form-control form-opacity" v-model="newAnswer.firstname">
                             </div>
                             <div class="col-md-6">
-                                <label v-html="$t('rsvp.form.lastname')"></label>
-                                <input type="text" name="lastname" class="form-control form-opacity">
+                                <label v-bind:for="'answerLastname_'+newAnswer['.key']" v-html="$t('rsvp.form.lastname')"></label>
+                                <input v-bind:id="'answerLastname_'+newAnswer['.key']" type="text" name="lastname" class="form-control form-opacity" v-model="newAnswer.lastname">
                             </div>
                             <div class="col-md-12">
-                                <label v-html="$t('rsvp.form.email')"></label>
-                                <input type="text" name="email" class="form-control form-opacity">
+                                <label v-bind:for="'answerEmail_'+newAnswer['.key']" v-html="$t('rsvp.form.email')"></label>
+                                <input v-bind:id="'answerEmail_'+newAnswer['.key']" type="text" name="email" class="form-control form-opacity"  v-model="newAnswer.email">
                             </div>
                             <div class="clear-both"></div> 
                             <div class="col-md-12">
                                 <label v-html="$t('rsvp.form.present')"></label>
-                                <label class="radio-inline"><input type="radio" name="attend_diner" value="1">{{ $t('rsvp.form.answer_yes')}}</label>
-                                <label class="radio-inline"><input type="radio" name="attend_diner" value="0">{{ $t('rsvp.form.answer_no')}}</label>
+                                <label class="radio-inline"><input type="radio" name="attend_diner" value="Oui" v-model="newAnswer.diner">{{ $t('rsvp.form.answer_yes')}}</label>
+                                <label class="radio-inline"><input type="radio" name="attend_diner" value="Non" v-model="newAnswer.diner">{{ $t('rsvp.form.answer_no')}}</label>
                             </div>
                             <div class="col-md-12">
                                 <label v-html="$t('rsvp.form.present')"></label>
-                                <label class="radio-inline"><input type="radio" name="attend_brunch" value="1">{{ $t('rsvp.form.answer_yes')}}</label>
-                                <label class="radio-inline"><input type="radio" name="attend_brunch" value="0">{{ $t('rsvp.form.answer_no')}}</label>
+                                <label class="radio-inline"><input type="radio" name="attend_brunch" value="Oui" v-model="newAnswer.brunch">{{ $t('rsvp.form.answer_yes')}}</label>
+                                <label class="radio-inline"><input type="radio" name="attend_brunch" value="Non" v-model="newAnswer.brunch">{{ $t('rsvp.form.answer_no')}}</label>
                             </div>
                             <div class="col-md-12">
                                 <label v-html="$t('rsvp.form.songs')"></label>
-                                <input type="text" name="dancing_song" class="form-control form-opacity">
+                                <input type="text" name="dancing_song" class="form-control form-opacity" v-model="newAnswer.songs">
                             </div>
                             <div class="col-md-12">
                                 <label v-html="$t('rsvp.form.notes')"></label>
-                                <textarea name="message" class="form-control form-opacity" :placeholder="$t('rsvp.form.hint_songs')"></textarea>
+                                <textarea name="message" class="form-control form-opacity" :placeholder="$t('rsvp.form.hint_notes')"  v-model="newAnswer.notes"></textarea>
                             </div>
                             <div class="col-md-12">
                                 <input type="submit" class="btn submit" :value="$t('rsvp.form.submit')">
                             </div>
                         </div>
                     </form>
+
                 </div>
             </div>
         </div>
@@ -60,44 +67,106 @@
 </style>
 
 <script>
-// import VueBootstrapTypeahead from 'vue-bootstrap-typeahead'
-// import {_} from 'vue-underscore'
-// import Vue from 'vue'
+import firebase from 'firebase'
+const fb = require('../../firebaseConfig.js')
 
-// const API_URL = 'https://api.deezer.com/search?q=:query'
+let db = fb.db;
+let answersRef = db.ref('answers')
 
+import toastr from 'toastr'
 
 export default {
-    components: {
-        
-    },
   name: 'Rsvp',
-  events: {
-  	
+  firebase: {
+    answers : answersRef
   },
-  data() {
-    return {
-      
-    }
+  components: {
   },
- 
   methods: {
+
+    addAnswer: function () {
+      answersRef.push(this.newAnswer);
+
+      if (this.$i18n.locale === 'fr'){
+        toastr.success('Merci '+this.newAnswer.firstname +" ! Nous avons bien pris en compte votre réponse !")
+      }else{
+        toastr.success('Thanks '+this.newAnswer.firstname +"! We successfully saved your answer!")
+      }
+
+
+      
+      this.newAnswer.firstname  = '';
+      this.newAnswer.lastname = '';
+      this.newAnswer.email = '';
+      this.newAnswer.diner = '';
+      this.newAnswer.brunch = '';
+      this.newAnswer.songs = '';
+      this.newAnswer.notes = '';
+
+
+
+    },
+
+    checkForm: function (e) {
+      if (this.newAnswer.lastname 
+        && this.newAnswer.firstname
+        && this.newAnswer.email
+        && this.newAnswer.diner  
+        && this.newAnswer.brunch
+        ) {
+        return addAnswer();
+      }
+
+      this.errors = [];
+
+      if (!this.newAnswer.firstname ) {
+        this.errors.push('Firstname required.');
+      }
+      if (!this.newAnswer.lastname) {
+        this.errors.push('Lastname required.');
+      }
+      if (!this.newAnswer.email) {
+        this.errors.push('Email required.');
+      }
+      if (!this.newAnswer.diner) {
+        this.errors.push('Anwser for the diner required.');
+      }
+      if (!this.newAnswer.brunch) {
+        this.errors.push('Anwser for the brunch required.');
+      }
+
+      e.preventDefault();
+    }
     
   },
- 
-  watch: {
-    
+  events: {
+    // https://vuejs.org/v2/guide/migration.html#dispatch-and-broadcast-replaced
+ //     'add-invite': function() {
+    //         this.$on('add-invite', function (child) {
+    //        // Make something there with child reference
+    //        console.log("reçu");
+    //        preventDefault();
+    //  })
+    // }
+  },
+  data () {
+    return {
+        newAnswer: {
+            lastname: '',
+            firstname: '',
+            email: '',
+            diner: '',
+            brunch: '',
+            songs : '',
+            notes : ''
+          },
+      editingInvite: null,
+      errors: []
+    }
   }
 }
-
-
-
- 
-
- 
-
-</script> 
-
-		
 </script>
+
+
+
 
